8913610f5ea286bfe6e4c355b50f99669510c997
trikitrok <trikitrok@gmail.com>
2015-10-02 17:04:57 +0200

    Reformatted code

--- WizardsAdventureGame/wizards-game.lisp
+++ WizardsAdventureGame/wizards-game.lisp
@@ -1,35 +1,34 @@
-(defparameter 
-  *nodes* 
-  '((living-room 
-     (you are in the living room.
-          a wizard is snoring loudly on the couch.))
-   (garden 
-     (you are in a beautiful garden.
-          there is a well in front of you.))
-   (attic 
-     (you are in the attic.
-          there is a giant welding torch in the corner.))))
+(defparameter
+  *nodes*
+  '((living-room
+      (you are in the living room.
+           a wizard is snoring loudly on the couch.))
+    (garden
+      (you are in a beautiful garden.
+           there is a well in front of you.))
+    (attic
+      (you are in the attic.
+           there is a giant welding torch in the corner.))))
 
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
-
-(defparameter 
+(defparameter
   *edges*
   '((living-room
       (garden west door)
       (attic upstairs ladder))
-    (garden 
+    (garden
       (living-room east door))
-    (attic 
+    (attic
       (living-room downstairs ladder))))
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
 
 (defun describe-paths (location edges)
-  (apply #'append 
-         (mapcar #'describe-path 
+  (apply #'append
+         (mapcar #'describe-path
                  (cdr (assoc location edges)))))
 
 (defparameter
@@ -44,20 +43,20 @@
     (frog garden)))
 
 (defun objects-at (loc objs obj-locs)
-  (labels 
+  (labels
     ((at-loc-p (obj)
-      (eq (cadr (assoc obj obj-locs)) loc))) 
+               (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
 
 (defun describe-objects (loc objs obj-locs)
-  (labels 
+  (labels
     ((describe-obj (obj)
-      `(you see a ,obj on the floor.))) 
-    (apply #'append 
-           (mapcar #'describe-obj 
+                   `(you see a ,obj on the floor.)))
+    (apply #'append
+           (mapcar #'describe-obj
                    (objects-at loc objs obj-locs)))))
 
-(defparameter 
+(defparameter
   *location*
   'living-room)
 
